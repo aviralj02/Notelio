7bde465aaa4cad16f71a4f378239a2e3afad64b1
aviralj02 <aviraljain1311@gmail.com>
2023-03-08 02:01:14 +0530

    add search component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import Search from '../public/assets/search-btn.png'
 
 type Props = {}
 
 const SearchBar = (props: Props) => {
   return (
     <div className='flex justify-center max-w-3xl mx-auto'>
-        <input 
-            type='text' 
-            placeholder='Search Notes...'
-            className='bg-[#1B1B1B] mt-[60px] w-[680px] h-[50px] border-2 border-[#2d2d2d] rounded-[13px] text-white placeholder:text-[#989898]'
-        />
+      <div className='relative mt-[60px]'>
+        <div className='absolute left-0 inset-y-0 flex items-center pl-5 pointer-events-none'>
+            <img 
+              src={Search.src}  
+              alt="search"
+              className='w-[14px] h-[14px]'
+            />
+          </div>
+          <input 
+              type='search' 
+              placeholder='Search Notes...'
+              className='block bg-[#1B1B1B] pl-12 pr-2 w-[680px] h-[50px] border-2 border-[#2d2d2d] rounded-[13px] text-white placeholder:text-[#989898] focus:border-[#2d2d2d]'
+          />
+      </div>
     </div>
   )
 }
